6a6100c0916379cb848243fa22a669e5a1f25090
AsoTiesto <cute17852337569@gmail.com>
2017-08-21 12:50:15 +0800

    new one

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-sim_106\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"sim_106\"}" 'http://www.ehanlin.com.tw/event/api/Deploy'
+curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-sim_106\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"sim_106\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
