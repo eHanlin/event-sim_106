466846e926c3292201c2493e11dbd7855172ca66
AsoTiesto <cute17852337569@gmail.com>
2017-09-13 11:18:02 +0800

    調整deploy與gulpfile完整度

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-sim_106\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"sim_106\"}" 'http://test.ehanlin.com.tw/event/api/Deploy'
+test="http://test.ehanlin.com.tw/event/api/Deploy"
+production="http://www.ehanlin.com.tw/event/api/Deploy"
+repository="{\"Repository\":\"event-sim_106\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"sim_106\"}"
+contentTypeJson="Content-Type: application/json"
+
+case "${TRAVIS_TAG}" in
+*SNAPSHOT*)
+	echo "deploy to test"
+	curl -X POST -H "${contentTypeJson}" -d "${repository}" "${test}"
+	;;
+*)
+	echo "deploy to production"
+	curl -X POST -H "${contentTypeJson}" -d "${repository}" "${production}"
+	;;
+esac
