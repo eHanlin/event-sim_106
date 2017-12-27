f3e0114e96ce08cf03fa680e019abc42429749b8
AsoTiesto <cute17852337569@gmail.com>
2017-12-27 11:49:00 +0800

    更新排行榜顯示資訊

--- dist/js/event-sim-106.js
+++ dist/js/event-sim-106.js
@@ -50,7 +50,7 @@ $(function() {
             "<img src=" +
             rootPath +
             "/img/比試結束.png></a>" +
-            "<a class='btnB'>" +
+            "<a class='btnB rank'>" +
             "<img src=" +
             rootPath +
             "/img/王者排行.png></a>"
@@ -59,3 +59,68 @@ $(function() {
     });
   });
 });
+
+var ajaxGet = function(url, param, success, error) {
+  return $.ajax({
+    type: "GET",
+    url: url,
+    data: param,
+    contentType: "application/json; charset=UTF-8",
+    dataType: "json",
+    cache: false,
+    crossDomain: true,
+    success: success,
+    error: error
+  });
+};
+
+var blockFunc = function(info) {
+  $.blockUI({
+    message:
+      "<div class='leader-boards'><span class='spanA'>106學年模擬會考 第一次全科1-4冊<br><br>" +
+      info +
+      "</span>" +
+      "<img class='listImgA' src='./img/英雄榜背景.png'>" +
+      "</div>",
+    css: {
+      top: ($(window).height() - 490) / 2 + "px",
+      left: ($(window).width() - 790) / 2 + "px",
+      border: "1px solid rgb(95, 95, 95)",
+      borderRadius: "10%",
+      padding: "0px",
+      boxShadow: "rgb(135, 135, 135) 0px 0px 8px 2px",
+      width: "790",
+      height: "490",
+      cursor: "default"
+    },
+    overlayCSS: { backgroundColor: "#000", opacity: "0.4" },
+    onOverlayClick: $.unblockUI
+  });
+};
+
+var clickBtnB = function() {
+  $(".btnB.rank").on("click", function() {
+    ajaxGet(
+      "https://www.ehanlin.com.tw/sim_106/hero?year=106&volume=7&number=1",
+      null,
+      function(jsonData) {
+        let info;
+
+        for (let index = 0; index < jsonData.length; index++) {
+          var userName = jsonData[index].userName;
+          var userSchool = jsonData[index].school;
+
+          console.log(jsonData[index].userName);
+          console.log(jsonData[index].school);
+
+          let userInfoList = `<span class="number">第${index +
+            1}名</span><p class='userList'> ${userName}&nbsp;&nbsp;${userSchool}</p>`;
+          info += userInfoList;
+          blockFunc(info.replace("undefined", ""));
+        }
+      },
+      function() {}
+    );
+  });
+};
+clickBtnB();
