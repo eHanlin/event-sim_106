e14108ec21ba399766157c49133a5f4550cf7d2f
AsoTiesto <cute17852337569@gmail.com>
2018-02-22 11:03:27 +0800

    follow #3448 更新模擬會考頁面圖

--- src/js/event-sim-106.js
+++ src/js/event-sim-106.js
@@ -1,111 +1,111 @@
-$(function() {
-  var now = new Date();
-  var rootPath = document.getElementById("rootPath").getAttribute("data-value");
-  var getBoard = function() {
-    var $this = $(this);
-    var year = $this.data("year");
-    var volume = $this.data("volume");
-    var number = $this.data("number");
-    Sim.getBoard(year, volume, number);
-  };
+$(function () {
+  var now = new Date()
+  var rootPath = document.getElementById('rootPath').getAttribute('data-value')
+  var getBoard = function () {
+    var $this = $(this)
+    var year = $this.data('year')
+    var volume = $this.data('volume')
+    var number = $this.data('number')
+    Sim.getBoard(year, volume, number)
+  }
 
-  var ajaxGet = function(url, param, success, error) {
+  var ajaxGet = function (url, param, success, error) {
     return $.ajax({
-      type: "GET",
+      type: 'GET',
       url: url,
       data: param,
-      contentType: "application/json; charset=UTF-8",
-      dataType: "json",
+      contentType: 'application/json; charset=UTF-8',
+      dataType: 'json',
       cache: false,
       crossDomain: true,
       success: success,
       error: error
-    });
-  };
+    })
+  }
 
-  var blockFunc = function(info) {
+  var blockFunc = function (info) {
     $.blockUI({
       message:
         "<div class='leader-boards'><span class='spanA'>106學年模擬會考 第一次全科1-4冊<br><br>" +
         info +
-        "</span>" +
+        '</span>' +
         "<img class='listImgA' src='" +
         rootPath +
         "/img/英雄榜背景.png'>" +
-        "</div>",
+        '</div>',
       css: {
-        top: ($(window).height() - 490) / 2 + "px",
-        left: ($(window).width() - 790) / 2 + "px",
-        border: "1px solid rgb(95, 95, 95)",
-        borderRadius: "10%",
-        padding: "0px",
-        boxShadow: "rgb(135, 135, 135) 0px 0px 8px 2px",
-        width: "790",
-        height: "490",
-        cursor: "default"
+        top: ($(window).height() - 490) / 2 + 'px',
+        left: ($(window).width() - 790) / 2 + 'px',
+        border: '1px solid rgb(95, 95, 95)',
+        borderRadius: '10%',
+        padding: '0px',
+        boxShadow: 'rgb(135, 135, 135) 0px 0px 8px 2px',
+        width: '790',
+        height: '490',
+        cursor: 'default'
       },
-      overlayCSS: { backgroundColor: "#000", opacity: "0.4" },
+      overlayCSS: { backgroundColor: '#000', opacity: '0.4' },
       onOverlayClick: $.unblockUI
-    });
-  };
+    })
+  }
 
   // let rootPath =
   //   "https://s3-ap-northeast-1.amazonaws.com/ehanlin-web-resource/event-sim_106/0.4.0-SNAPSHOT";
-  Sim.getTermtest(106, "complete", function(schedule) {
-    _.each(schedule, function(it, idx) {
+  Sim.getTermtest(106, 'complete', function (schedule) {
+    _.each(schedule, function (it, idx) {
       if (now < it.start) {
-        $(".block" + idx).append(
+        $('.block' + idx).append(
           `
           <a class='btnA'>
           <img src='${rootPath}/img/尚未開始.png'></a>
           <a class='btnB'>
           <img src='${rootPath}/img/尚未公布.png'></a>
           `
-        );
+        )
       } else if (now > it.start && now < it.end) {
-        $(".block" + idx).append(`
+        $('.block' + idx).append(`
           <a class='btnA' href='/key/MOCK106FEXAM/SalesPlans/productSets.html?check=false'>
             <img style="hover:box-shadow: 2px 2px 1px grey;" src='${rootPath}/img/進入擂台.png'></a> 
           <a class='btnB'>
             <img src='${rootPath}/img/尚未公布.png'></a>
-          `);
+          `)
       } else if (now > it.end && now < it.result) {
-        $(".block" + idx).append(
+        $('.block' + idx).append(
           `
           <a class='btnA'>
           <img src='${rootPath}/img/比試結束.png'></a>
           <a class='btnB'>
           <img src='${rootPath}/img/尚未公布.png'></a>
           `
-        );
+        )
       } else if (now > it.result) {
-        $(".block" + idx).append(`
+        $('.block' + idx).append(`
           <a class='btnA'>
           <img src='${rootPath}/img/比試結束.png'></a>
           <a class='btnB'>
           <img style="hover:box-shadow: 2px 2px 1px grey;" src='${rootPath}/img/王者排行.png'></a>
-          `);
+          `)
       }
-    });
+    })
 
-    $(".block0 .btnB img").on("click", function() {
+    $('.block0 .btnB img').on('click', function () {
       ajaxGet(
-        "https://www.ehanlin.com.tw/sim_106/hero?year=106&volume=7&number=1",
+        'https://www.ehanlin.com.tw/sim_106/hero?year=106&volume=7&number=1',
         null,
-        function(jsonData) {
-          let info;
+        function (jsonData) {
+          let info
 
           for (let index = 0; index < jsonData.length; index++) {
-            var userName = jsonData[index].userName;
-            var userSchool = jsonData[index].school;
+            var userName = jsonData[index].userName
+            var userSchool = jsonData[index].school
             let userInfoList = `<span class="number">第${index +
-              1}名</span><p class='userList'> ${userName}&nbsp;&nbsp;${userSchool}</p>`;
-            info += userInfoList;
-            blockFunc(info.replace("undefined", ""));
+              1}名</span><p class='userList'> ${userName}&nbsp;&nbsp;${userSchool}</p>`
+            info += userInfoList
+            blockFunc(info.replace('undefined', ''))
           }
         },
-        function() {}
-      );
-    });
-  });
-});
+        function () {}
+      )
+    })
+  })
+})
